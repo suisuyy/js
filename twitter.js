60593843e44c300e74d70e56125e4049c7b71f9c
suisuyy <suisuyus@gmail.com>
2023-12-21 00:10:42 +0800

    Update twitter.js

--- twitter.js
+++ twitter.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         dischinese
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  auto hide chinese on twetter and auto translate
+// @author       imvilent
 // @match        https://*twitter.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
 // @grant        none
